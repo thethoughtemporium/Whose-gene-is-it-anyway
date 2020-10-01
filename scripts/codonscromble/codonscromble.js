91fb007f6050d07880825382dc66619f4d984927
otdav33 <owen.davies.t@gmail.com>
2020-10-01 16:45:16 -0400

    Implemented converting the other way around in codonscrombler

--- scripts/codonscromble/codonscromble.js
+++ scripts/codonscromble/codonscromble.js
@@ -1,4 +1,5 @@
-function codonsToDNA() { var dict = JSON.parse(document.getElementById("codondict").value);
+function codonsToDNA() {
+  var dict = JSON.parse(document.getElementById("codondict").value);
   var input = document.getElementById("codons").value.replace(/\s/g,"");
   input = input.toUpperCase();
   var out = "";
@@ -6,7 +7,7 @@ function codonsToDNA() { var dict = JSON.parse(document.getElementById("codondic
   var cs = 0;
   var ts = 0;
   var gs = 0;
-  for (var i=0; i < input.length ; i++) {
+  for (var i=0; i < input.length; i++) {
     if (input[i] < "0" || input[i] > "9") {
       if (!(input[i] in dict)) {
         alert("The character \"" + input[i] + "\" at position " + i + " is invalid.");
@@ -22,12 +23,49 @@ function codonsToDNA() { var dict = JSON.parse(document.getElementById("codondic
         if (choices[chnum][j] == 't') { ts++; }
         if (choices[chnum][j] == 'g') { gs++; }
       }
-      document.getElementById("DNA").innerHTML = out;
     }
   }
+  document.getElementById("DNA").value = out;
   document.getElementById("analytics").innerHTML = "<table><tr><td>Letter</td>"
     + "<td>Occourences</td></tr><tr><td>a</td><td>" + as + "</td></tr>"
     + "<tr><td>c</td><td>" + cs + "</td></tr><tr><td>t</td><td>"
     + ts + "</td></tr><tr><td>g</td><td>" + gs + "</td></tr></table>"
     + "<p>GC ratio: " + ((gs + cs) / (as + ts) * 100) + "%</p>";
+}
+
+function DNAToCodons() {
+  var dict = JSON.parse(document.getElementById("codondict").value);
+  var input = document.getElementById("DNA").value.replace(/\s/g,"");
+  input = input.toLowerCase();
+  var out = "";
+  for (var i=0; i < input.length; i++) {
+    if (input[i] < "0" || input[i] > "9") {
+      if (!"actg".includes(input[i])) {
+        alert("The character \"" + input[i] + "\" at position " + i + " is invalid.");
+        return;
+      }
+    }
+  }
+  for (var i=0; i < input.length; i+=3) {
+    if (input[i] < "0" || input[i] > "9") {
+      var t = "" + input[i] + input[i+1] + input[i+2];
+      var c = undefined;
+      for (codon in dict) {
+        for (triplet in dict[codon]) {
+          if (t == dict[codon][triplet]) {
+            c = codon;
+          }
+        }
+      }
+      if (c == undefined) {
+        //we have not found any codon for the DNA triplet
+        alert("The DNA triplet \"" + t + "\" at position " + i + "is invalid.");
+        return;
+      }
+
+      out += c;
+    }
+  }
+  document.getElementById("codons").value = out;
+  document.getElementById("analytics").innerHTML = "";
 }
