bb3b4ad8e5c813a5bc2a5803daa91bf409d3f4b4
otdav33 <owen.davies.t@gmail.com>
2020-10-02 13:59:05 -0400

    Added a repetition counter analytic and fixed some typos

--- scripts/codonscromble/codonscromble.js
+++ scripts/codonscromble/codonscromble.js
@@ -1,6 +1,6 @@
 function codonsToDNA() {
   var dict = JSON.parse(document.getElementById("codondict").value);
-  var input = document.getElementById("codons").value.replace(/\s/g,"");
+  var input = document.getElementById("codons").value.replace(/[\s\d]/g,"");
   input = input.toUpperCase();
   var out = "";
   var as = 0;
@@ -8,34 +8,34 @@ function codonsToDNA() {
   var ts = 0;
   var gs = 0;
   for (var i=0; i < input.length; i++) {
-    if (input[i] < "0" || input[i] > "9") {
-      if (!(input[i] in dict)) {
-        alert("The character \"" + input[i] + "\" at position " + i + " is invalid.");
-        return;
-      }
-      var choices = dict[input[i]];
-      var chnum = Math.floor(Math.random() * choices.length);
+    if (!(input[i] in dict)) {
+      alert("The character \"" + input[i] + "\" at position " + i + " is invalid.");
+      return;
+    }
+    var choices = dict[input[i]];
+    var chnum = Math.floor(Math.random() * choices.length);
 
-      out += choices[chnum];
-      for (var j=0; j < choices[chnum].length; j++) {
-        if (choices[chnum][j] == 'a') { as++; }
-        if (choices[chnum][j] == 'c') { cs++; }
-        if (choices[chnum][j] == 't') { ts++; }
-        if (choices[chnum][j] == 'g') { gs++; }
-      }
+    out += choices[chnum];
+    for (var j=0; j < choices[chnum].length; j++) {
+      if (choices[chnum][j] == 'a') { as++; }
+      if (choices[chnum][j] == 'c') { cs++; }
+      if (choices[chnum][j] == 't') { ts++; }
+      if (choices[chnum][j] == 'g') { gs++; }
     }
   }
   document.getElementById("DNA").value = out;
-  document.getElementById("analytics").innerHTML = "<table><tr><td>Letter</td>"
+
+  var analytics = "<table><tr><td>Letter</td>"
     + "<td>Occourences</td></tr><tr><td>a</td><td>" + as + "</td></tr>"
     + "<tr><td>c</td><td>" + cs + "</td></tr><tr><td>t</td><td>"
     + ts + "</td></tr><tr><td>g</td><td>" + gs + "</td></tr></table>"
     + "<p>GC ratio: " + ((gs + cs)/(as+ts+gs+cs) * 100) + "%</p>";
+  document.getElementById("analytics").innerHTML = analytics; 
 }
 
 function DNAToCodons() {
   var dict = JSON.parse(document.getElementById("codondict").value);
-  var input = document.getElementById("DNA").value.replace(/\s/g,"");
+  var input = document.getElementById("DNA").value.replace(/[\s\d]/g,"");
   input = input.toLowerCase();
   var out = "";
   for (var i=0; i < input.length; i++) {
@@ -68,4 +68,39 @@ function DNAToCodons() {
   }
   document.getElementById("codons").value = out;
   document.getElementById("analytics").innerHTML = "";
+}
+
+function checkForRepeats() {
+  var analytics = "<table><tr><td>Sequence</td><td>Occourences</td></tr>";
+  var dna = document.getElementById("DNA").value.replace(/[\s\d]/g,"");
+
+  var repDict = {}; //count the repetitions of all occourances 10+bp long
+  for (var r = 30; r >= 10; r--) {
+    //dna.length/2 is picked as things have to repeat at least twice.
+    for (var i = 0; i + r < dna.length; i++) {
+      let selection = dna.slice(i, i + r);
+      //increment the count of the selection
+      if (repDict[selection] == undefined) {
+        repDict[selection] = 1;
+      } else {
+        repDict[selection] += 1;
+      }
+    }
+    for (sequence in repDict) {
+      //delete all sequences that do not repeat
+      if (repDict[sequence] == 1) {
+        delete repDict[sequence];
+      }
+    }
+  }
+  for (sequence in repDict) {
+    if (repDict[sequence] > 1) {
+      //list in analytics table
+      analytics += "<tr><td>" + sequence + "</td><td>" + repDict[sequence]
+        + "</td></tr>";
+    }
+  }
+  analytics += "</table>";
+    
+  document.getElementById("repeats").innerHTML = analytics; 
 }
